20034a06324e17cda61e43b8c23e6527042aeb3f
corno <corno.astn.a@schraverus.com>
2023-03-01 20:29:15 +0100

    .

--- src/index.ts
+++ src/index.ts
@@ -13,29 +13,34 @@ cp.exec(
             console.error(`${stderr}`)
             process.exit(1)
         } else {
-            Object.keys(JSON.parse(stdout)).forEach((key) => {
-                cp.exec(
-                    `npm view ${key}@latest version`,
-                    (err, stdout, stderr) => {
+            const dependencies = Object.keys(JSON.parse(stdout))
+            function doNext() {
+                const next = dependencies.pop()
+                if (next !== undefined) {
+                    const key = next
+    
+                    //console.log(`KEY: ${key}`)
+                    cp.exec(`npm view ${key}@latest version`, (err, stdout, stderr) => {
                         if (err !== null) {
-                            console.error(`${stderr}`)
-                            process.exit(1)
+                            console.error(`could not retrieve latest version: ${stderr}`);
+                            process.exit(1);
                         } else {
-                            cp.exec(
-                                `npm pkg set "dependencies.${key}"="^${stdout.trimEnd()}" --prefix ${contextDir}`,
-                                (err, stdout, stderr) => {
-                                    if (err !== null) {
-                                        console.error(`${stderr}`)
-                                        process.exit(1)
-                                    } else {
-                                        //nothing to do
-                                    }
+                            //console.log(`KEY VERSION: ${key}:${stdout.trimEnd()}`)
+                            cp.exec(`npm pkg set "dependencies.${key}"="^${stdout.trimEnd()}" --prefix ${contextDir}`, (err, stdout, stderr) => {
+                                //console.log(`KEY SET: ${key}`)
+                                if (err !== null) {
+                                    console.error(`could not set dependency version: ${stderr}`);
+                                    process.exit(1);
                                 }
-                            )
+                                else {
+                                    doNext()
+                                }
+                            });
                         }
-                    }
-                )
-            })
+                    });
+                }
+            }
+            doNext()
         }
     }
 )
