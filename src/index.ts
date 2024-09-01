e44917230b1fe80f4c2c45b70ad0fe50989abd2e
Corno Schraverus <corno@schraverus.com>
2024-09-01 16:37:27 +0200

    .

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 import * as cp from "child_process"
+import { argv, cwd } from "process"
 
 if (process.argv.length < 4) {
     console.error("usage: 'directory containing the package.json' dependencies|devDependencies [verbose]")
@@ -20,11 +21,19 @@ if (process.argv[4] !== undefined && process.argv[4] !== "verbose" ) {
     process.exit(1)
 }
 
-
-const verbose = process.argv[4] !== undefined
+const beVerbose = process.argv[4] !== undefined
 
 cp.exec(
     `npm pkg get "${dependencyType}" --prefix ${contextDir}`,
+    /*
+    should return something like this:
+
+    {
+        "@types/node": "^22.4.0",
+        "typescript": "^5.5.4"
+    }
+
+    */
     (err, stdout, stderr) => {
         if (err !== null) {
             console.error(`${stderr}`)
@@ -33,22 +42,33 @@ cp.exec(
             try {
                 JSON.parse(stdout)
             } catch (e) {
+                console.error(`error while executing ${argv[1]}`)
+                console.error(`working directory ${cwd()}`)
                 console.error(`the command that was run: npm pkg get "${dependencyType}" --prefix ${contextDir}`)
                 console.error(`the following is not valid JSON: '${stdout}'`)
                 process.exit(1)
             }
+            /* we have a valid JSON */
             const dependencies = Object.keys(JSON.parse(stdout))
             if (dependencies.length === 0) {
-                if (verbose) {
+                if (beVerbose) {
                     console.log("-no dependencies-")
                 }
             } else {
                 const versions: [string, string][] = []
+
                 function push(key: string, version: string) {
                     versions.push([key, version])
+                    /*
+                    every version check is an asynchronous process, so with every result we need to check
+                    if all versions are processed, if so, do the final step
+                     */
                     if (versions.length === dependencies.length) {
                         cp.exec(`npm pkg set ${versions.map(($) => `${dependencyType}.${$[0]}="^${$[1]}"`).join(" ")} --prefix ${contextDir}`, (err, stdout, stderr) => {
-                            if (verbose) {
+                            /*
+                            updates the version numbers in the package.json file
+                            */
+                            if (beVerbose) {
                                 versions.forEach(($) => {
                                     console.log(`${$[0]}:${$[1]}`)
                                 })
@@ -62,8 +82,10 @@ cp.exec(
                     }
                 }
                 dependencies.forEach((key) => {
-                    //console.log(`KEY: ${key}`)
                     cp.exec(`npm view ${key}@latest version`, (err, stdout, stderr) => {
+                        /*
+                        gets the latest version from the online database
+                        */
                         if (err !== null) {
                             console.error(`could not retrieve latest version: ${stderr}`);
                             process.exit(1);
